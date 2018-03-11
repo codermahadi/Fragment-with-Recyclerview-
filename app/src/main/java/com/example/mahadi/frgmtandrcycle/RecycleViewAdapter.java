34d86f2ece7a8ae5a67d0bdbbd7a2a1d7d0d48c9
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-03-11 10:40:23 +0600

    create recycle view adapter class

---
+++ app/src/main/java/com/example/mahadi/frgmtandrcycle/RecycleViewAdapter.java
@@ -0,0 +1,60 @@
+package com.example.mahadi.frgmtandrcycle;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Mahadi on 3/11/2018.
+ */
+
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.MyViewHolder>{
+
+    Context context;
+    List<Contact> contactList;
+
+    public RecycleViewAdapter(Context context, List<Contact> contactList) {
+        this.context = context;
+        this.contactList = contactList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v;
+        v = LayoutInflater.from(context).inflate(R.layout.item_contact,parent,false);
+        MyViewHolder myViewHolder = new MyViewHolder(v);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView name;
+        TextView phone_num;
+        ImageView imageView;
+
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            name  = (TextView) itemView.findViewById(R.id.name_contact);
+            phone_num = (TextView)itemView.findViewById(R.id.ph_number);
+            imageView = (ImageView)itemView.findViewById(R.id.img);
+        }
+    }
+}
