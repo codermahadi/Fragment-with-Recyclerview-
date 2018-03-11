0a7b9825c84232384dc1ea3e0f0d47408149e17d
codermahadi <31430772+codermahadi@users.noreply.github.com>
2018-03-11 10:35:13 +0600

    create Contact java file and add getter setter

---
+++ app/src/main/java/com/example/mahadi/frgmtandrcycle/Contact.java
@@ -0,0 +1,42 @@
+package com.example.mahadi.frgmtandrcycle;
+
+/**
+ * Created by Mahadi on 3/11/2018.
+ */
+
+public class Contact {
+
+     private String name;
+    private String phn;
+    private int photo;
+
+    public Contact(String name, String phn, int photo) {
+        this.name = name;
+        this.phn = phn;
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhn() {
+        return phn;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhn(String phn) {
+        this.phn = phn;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+}
